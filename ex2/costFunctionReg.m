ded782bd51cc2d7899fc3518b241dd5a15bc834c
Antoine <karagan.app@gmail.com>
2020-01-20 13:46:48 +0100

    adding regularization to cost function

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -19,10 +19,12 @@ grad = zeros(size(theta));
 
 
 h = sigmoid(X*theta);
+theta_without_zero = [0;theta(2:size(theta))];
 
-J = 1/(m) * sum(-y'*log(h) - (1 .- y)'*log(1 - h)) + lambda/(2*m) * sum(theta(2:size(theta)).^2);
+J = 1/(m) * sum(-y'*log(h) - (1 - y)'*log(1 - h)) + lambda/(2*m) * sum(theta_without_zero.^2);
 
-grad = (1/m) * ((h - y)'*X + lambda*[0;theta(2:size(theta))]);
+grad = (1/m) * (X'*(h-y) + lambda*theta_without_zero);
+
 
 % =============================================================
 
